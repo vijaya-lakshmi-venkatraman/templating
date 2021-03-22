f1333d4c0fd97e0fc10b47a1e01b868130ec60e4
Vlada Shubina <vshubina@microsoft.com>
2021-03-22 16:08:42 +0200

    extracted helper methods to Microsoft.TemplateEngine.TestHelper

--- test/Microsoft.TemplateEngine.IDE.IntegrationTests/TemplatePackagesTests.cs
+++ test/Microsoft.TemplateEngine.IDE.IntegrationTests/TemplatePackagesTests.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +10,7 @@ using FluentAssertions;
 using Microsoft.TemplateEngine.Abstractions.Installer;
 using Microsoft.TemplateEngine.Abstractions.TemplatesSources;
 using Microsoft.TemplateEngine.IDE.IntegrationTests.Utils;
+using Microsoft.TemplateEngine.TestHelper;
 using Xunit;
 
 namespace Microsoft.TemplateEngine.IDE.IntegrationTests
@@ -107,7 +107,7 @@ namespace Microsoft.TemplateEngine.IDE.IntegrationTests
         internal async Task CanInstall_Folder()
         {
             Bootstrapper bootstrapper = BootstrapperFactory.GetBootstrapper();
-            string templateLocation = TestHelper.GetTestTemplateLocation("TemplateWithSourceName");
+            string templateLocation = TestUtils.GetTestTemplateLocation("TemplateWithSourceName");
 
             InstallRequest installRequest = new InstallRequest
             {
